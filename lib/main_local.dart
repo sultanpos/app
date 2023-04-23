c8843e15f2a25bfe9bbde1a556191c0dc846257d
apin <apin.klas@gmail.com>
2023-04-23 20:54:49 +0700

    adding le http

--- lib/main_local.dart
+++ lib/main_local.dart
@@ -1,10 +1,23 @@
+import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter/material.dart';
 import 'package:sultanpos/app.dart';
 import 'package:sultanpos/flavor.dart';
+import 'package:sultanpos/ui/theme.dart';
+import 'dart:io' show Platform;
 
 Future<void> main() async {
   Flavor.appFlavor = FlavorType.development;
-  Flavor.baseUrl = "http://localhost:6789";
+  Flavor.baseUrl = "http://172.30.191.65:6789";
   WidgetsFlutterBinding.ensureInitialized();
+  STheme().init();
   runApp(const App());
+  if (Platform.isWindows) {
+    doWhenWindowReady(() {
+      const initialSize = Size(1280, 720);
+      appWindow.minSize = initialSize;
+      appWindow.size = initialSize;
+      appWindow.alignment = Alignment.center;
+      appWindow.show();
+    });
+  }
 }
