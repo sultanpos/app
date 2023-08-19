0eb734392bc3a079067a0a5a57f4a9d94a80d5bf
apin <apin.klas@gmail.com>
2023-08-19 16:32:09 +0700

    fix compile android

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -1,11 +1,27 @@
+import 'dart:io';
+
+import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter/material.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:sultanpos/app.dart';
 import 'package:sultanpos/flavor.dart';
+import 'package:sultanpos/ui/theme.dart';
 
 Future<void> main() async {
   Flavor.appFlavor = FlavorType.production;
   Flavor.baseUrl = "https://sultanpos-api.lekapin.com";
   Flavor.baseUrlWs = "wss://sultanpos-api.lekapin.com";
   WidgetsFlutterBinding.ensureInitialized();
+  STheme().init();
+  sqfliteFfiInit();
   runApp(const App());
+  if (Platform.isWindows) {
+    doWhenWindowReady(() {
+      const initialSize = Size(1280, 720);
+      appWindow.minSize = initialSize;
+      appWindow.size = initialSize;
+      appWindow.alignment = Alignment.center;
+      appWindow.show();
+    });
+  }
 }
