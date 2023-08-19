66c494234f295370b6d46ad91bca2e87fb5cd4db
apin <apin.klas@gmail.com>
2023-08-19 09:52:19 +0700

    dismiss other toast

--- lib/ui/widget/showerror.dart
+++ lib/ui/widget/showerror.dart
@@ -3,46 +3,50 @@ import 'package:oktoast/oktoast.dart';
 
 showError(BuildContext ctx, {required String message}) {
   showToastWidget(
-      Container(
-        width: 300,
-        padding: const EdgeInsets.all(8),
-        decoration: BoxDecoration(
-          color: Colors.red.withAlpha(200),
-          borderRadius: const BorderRadius.all(Radius.circular(16)),
-        ),
-        child: Row(children: [
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8),
-            child: Icon(
-              Icons.warning,
-              color: Colors.white,
-            ),
-          ),
-          Expanded(child: Text(message)),
-        ]),
+    Container(
+      width: 300,
+      padding: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        color: Colors.red.withAlpha(200),
+        borderRadius: const BorderRadius.all(Radius.circular(16)),
       ),
-      position: ToastPosition.top);
+      child: Row(children: [
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8),
+          child: Icon(
+            Icons.warning,
+            color: Colors.white,
+          ),
+        ),
+        Expanded(child: Text(message)),
+      ]),
+    ),
+    position: ToastPosition.top,
+    dismissOtherToast: true,
+  );
 }
 
 showSuccess(BuildContext ctx, {required String message}) {
   showToastWidget(
-      Container(
-        width: 300,
-        padding: const EdgeInsets.all(8),
-        decoration: BoxDecoration(
-          color: Colors.green.withAlpha(200),
-          borderRadius: const BorderRadius.all(Radius.circular(16)),
-        ),
-        child: Row(children: [
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8),
-            child: Icon(
-              Icons.check,
-              color: Colors.white,
-            ),
-          ),
-          Expanded(child: Text(message)),
-        ]),
+    Container(
+      width: 300,
+      padding: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        color: Colors.green.withAlpha(200),
+        borderRadius: const BorderRadius.all(Radius.circular(16)),
       ),
-      position: ToastPosition.top);
+      child: Row(children: [
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8),
+          child: Icon(
+            Icons.check,
+            color: Colors.white,
+          ),
+        ),
+        Expanded(child: Text(message)),
+      ]),
+    ),
+    position: ToastPosition.top,
+    dismissOtherToast: true,
+  );
 }
